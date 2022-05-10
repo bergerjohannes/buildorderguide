8534c65d945223927e4c0528ac983cf2850b0ff4
Johannes Berger <johannes@berger.ac>
2022-05-10 23:28:51 +0200

    Load builds and display them + add all required images

---
+++ src/Builds/AttributesView.js
@@ -0,0 +1,33 @@
+const AttributesView = (props) => {
+
+    const getAttribute = (attribute) => {
+        switch (attribute) {
+            case 'drush':
+                return 'Drush'
+            case 'fastFeudal':
+                return 'Fast Feudal'
+            case 'fastCastle':
+                return 'Fast Castle'
+            case 'fastImperial':
+                return 'Fast Imperial'
+            case 'water':
+                return 'Water/Hybrid'
+            case 'arena':
+                return 'Arena'
+            case 'meme':
+                return 'Meme'
+            default:
+                return 'Unknown'
+        }
+    }
+    
+    return (
+        <div class='flex space-x-2'>
+            {props.attributes !== undefined && props.attributes.map(attribute => (
+                <div class='bg-slate-300 text-gray-600 px-2 rounded-md'>{getAttribute(attribute)}</div>
+            ))}
+        </div>
+    )
+}
+
+export default AttributesView
