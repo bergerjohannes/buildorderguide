d9949746318f71117519e94d39787486d1a6f9a3
Johannes Berger <johannes@berger.ac>
2022-06-16 23:31:23 +0200

    Design improvements

--- src/UI/Logo.js
+++ src/UI/Logo.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const Logo = (props) => {
     return(
-        <Link to='/'><div class='uppercase font-semibold tracking-wider text-sm py-1 px-3 m-4 flex z-50'><img class='w-5 h-5' src={require('../Images/Logo.png')} /><span class='pl-1'>Build Order Guide</span></div></Link>
+        <Link to='/'><div class='sticky uppercase font-semibold tracking-wider text-sm py-1 px-3 m-4 flex z-50'><img class='w-5 h-5' src={require('../Images/Logo.png')} /><span class='pl-1'>Build Order Guide</span></div></Link>
     )
 }
 
