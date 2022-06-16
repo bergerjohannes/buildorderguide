cd8c4ca6d7fdfdf6649dbe8bb02c20a3d983bb99
Johannes Berger <johannes@berger.ac>
2022-06-16 23:31:23 +0200

    Design improvements

--- src/UI/Menu.js
+++ src/UI/Menu.js
@@ -11,7 +11,7 @@ const Menu = (props) => {
     isMenuOpen ? disableBodyScroll(document) : enableBodyScroll(document)
 
     return (
-        <div class='sticky top-0 flex justify-between z-50 bg-secondary-light'>
+        <div class='sticky top-0 flex justify-between z-30 bg-secondary-light'>
             <Logo/>
             <div class={isMenuOpen ? 'flex' : 'hidden md:flex'}>
                 <nav class='flex flex-col md:flex-row justify-center md:justify-end md:space-x-4 space-y-6 md:space-y-0 min-h-screen md:min-h-fit fixed z-10 md:relative items-center w-full md:w-auto bg-secondary-light md:bg-transparent left-0 top-0 m-0 md:m-4 p-0'>
@@ -22,8 +22,8 @@ const Menu = (props) => {
                     <MenuItem linkTo='/profile'>Profile</MenuItem>
                 </nav>
             </div>
-            <button class={isMenuOpen ? 'hidden' : 'block md:hidden z-50 m-4'} onClick={() => setIsMenuOpen(!isMenuOpen)}><FontAwesomeIcon icon={faBars} /></button>
-            <button class={!isMenuOpen ? 'hidden' : 'block md:hidden fixed z-50 right-0 m-4'} onClick={() => setIsMenuOpen(!isMenuOpen)}><FontAwesomeIcon icon={faXmark} /></button>
+            <button class={isMenuOpen ? 'hidden' : 'block md:hidden z-40 m-4'} onClick={() => setIsMenuOpen(!isMenuOpen)}><FontAwesomeIcon icon={faBars} /></button>
+            <button class={!isMenuOpen ? 'hidden' : 'block md:hidden fixed z-40 right-0 m-4'} onClick={() => setIsMenuOpen(!isMenuOpen)}><FontAwesomeIcon icon={faXmark} /></button>
         </div>
     )
 }
