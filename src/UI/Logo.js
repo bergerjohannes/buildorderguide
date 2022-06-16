60c66ae800e49044b4a34e17904543c98946fbbc
Johannes Berger <johannes@berger.ac>
2022-06-16 23:23:40 +0200

    Style uptime view + small design improvements for build overview and detail view

--- src/UI/Logo.js
+++ src/UI/Logo.js
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom'
+
 const Logo = (props) => {
     return(
-        <div class='uppercase font-semibold tracking-wider text-sm py-1 px-3 m-4 flex z-50'><img class='w-5 h-5' src={require('../Images/Logo.png')} /><span class='pl-1'>Build Order Guide</span></div>
+        <Link to='/'><div class='uppercase font-semibold tracking-wider text-sm py-1 px-3 m-4 flex z-50'><img class='w-5 h-5' src={require('../Images/Logo.png')} /><span class='pl-1'>Build Order Guide</span></div></Link>
     )
 }
 
