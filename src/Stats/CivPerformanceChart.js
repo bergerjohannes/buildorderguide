2653488c6ec03a015e4402c57c0c149232f06851
Johannes Berger <johannes@berger.ac>
2022-06-17 23:49:46 +0200

    Continue styling

--- src/Stats/CivPerformanceChart.js
+++ src/Stats/CivPerformanceChart.js
@@ -1,30 +1,34 @@
+import { Chart as ChartJS } from 'chart.js/auto'
 import { Scatter } from 'react-chartjs-2'
 import CivInfoService from '../Uptime/CivInfoService.js'
 
 const CivPerformanceChart = (props) => {
 
+    const prepareData = () => {
+        let dataPoints = []
+        let dataLabels = []
+        let dataImages = []
 
-    let dataPoints = []
-    let dataLabels = []
-    let dataImages = []
+        props.data.forEach((element, index) => {
+            dataPoints.push({ x: element.games, y: parseInt(element.winPercentage) })
+            dataLabels.push(CivInfoService.getCivilizationNameForIndex(index))
+            let img = new Image(22, 22)
+            img.src = require('../Images/Civilizations/' + CivInfoService.getCivilizationNameForIndex(index) + '.png')
+            dataImages.push(img)
+        })
 
-    props.data.forEach((element, index) => {
-        dataPoints.push({ x: element.games, y: parseInt(element.winPercentage) })
-        dataLabels.push(CivInfoService.getCivilizationNameForIndex(index))
-        let img = new Image(22, 22)
-        img.src = require('../Images/Civilizations/' + CivInfoService.getCivilizationNameForIndex(index) + '.png')
-        dataImages.push(img)
-    })
+        const data = {
+            datasets: [
+                {
+                    label: 'Civ Performance',
+                    labels: dataLabels,
+                    data: dataPoints,
+                    pointStyle: dataImages
+                }
+            ]
+        }
 
-    const data = {
-        datasets: [
-            {
-                label: 'Civ Performance',
-                labels: dataLabels,
-                data: dataPoints,
-                pointStyle: dataImages
-            }
-        ]
+        return data
     }
 
     const options = {
@@ -35,15 +39,37 @@ const CivPerformanceChart = (props) => {
         scales: {
             y: {
                 grid: {
-                    drawBorder: false
+                    drawBorder: false,
+                    display: true,
+                    color: "#f1f5f9"
                 },
                 ticks: {
-                    stepSize: 10
+                    color: "#134e4a",
+                    min: 0,
+                    max: 100,
+                    stepSize: 20
                 },
-                beginAtZero: true
+                offset: true
+            },
+            x: {
+                grid: {
+                    drawBorder: false,
+                    display: true,
+                    color: '#f1f5f9'
+                },
+                ticks: {
+                    color: "#134e4a"
+                },
+                offset: true
             }
         },
+        layout: {
+            overflow: 'scroll'
+        },
         plugins: {
+            legend: {
+                display: false
+            },
             tooltip: {
                 callbacks: {
                     label: function (ctx) {
@@ -59,7 +85,7 @@ const CivPerformanceChart = (props) => {
     if (props.data === undefined || props.data.length === 0) {
         return <></>
     } else {
-        return <Scatter data={data} options={options} />
+        return <Scatter data={prepareData()} options={options} />
     }
 }
 
--- src/Stats/RatingsGraph.js
+++ src/Stats/RatingsGraph.js
@@ -1,12 +1,44 @@
 
-import Chart from 'chart.js/auto'
+import { useEffect, useRef, useState } from 'react'
+import { Chart as ChartJS } from 'chart.js/auto'
 import { Line } from 'react-chartjs-2'
 
 const RatingsGraph = (props) => {
 
-    const graphOptions = {
-        legend: {
-            display: false
+    const chartRef = useRef(null)
+    const [chartData, setChartData] = useState({
+        datasets: []
+    })
+
+    useEffect(() => {
+
+        const chart = chartRef.current
+        if (!chart) return
+
+        const data = prepareData()
+
+        const chartData = {
+            ...data,
+            datasets: data.datasets.map(dataset => ({
+                ...dataset,
+                label: 'Elo',
+                borderWidth: 2,
+                pointRadius: 0,
+                fill: 'start',
+                tension: 1,
+                borderColor: '#2dd4bf',
+                backgroundColor: createGradient(chart.ctx, chart.chartArea),
+            })),
+        }
+
+        setChartData(chartData)
+    }, [])
+
+    const chartOptions = {
+        plugins: {
+            legend: {
+                display: false
+            }
         },
         maintainAspectRatio: false,
         scales: {
@@ -15,7 +47,8 @@ const RatingsGraph = (props) => {
                     display: false
                 },
                 ticks: {
-                    maxTicksLimit: 14
+                    maxTicksLimit: 14,
+                    color: "#134e4a"
                 }
             },
             y: {
@@ -24,40 +57,43 @@ const RatingsGraph = (props) => {
                     drawBorder: false
                 },
                 ticks: {
-                    stepSize: 100
+                    stepSize: 100,
+                    color: "#134e4a"
                 },
                 beginAtZero: false
             }
         }
     }
 
-    let dataPoints = []
-    let dataLabels = []
+    const createGradient = (ctx, area) => {
+        const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top)
 
-    props.data.forEach(element => {
-        dataPoints.push(element.Elo)
-        dataLabels.push(element.time)
-    })
+        gradient.addColorStop(0, '#ffffff')
+        gradient.addColorStop(1, '#2dd4bf')
 
-    const data = {
-        labels: dataLabels,
-        datasets: [
-            {
-                label: 'Elo',
-                data: dataPoints,
-                borderWidth: 2,
-                pointRadius: 0,
-                fill: 'start',
-                tension: 1,
-                borderColor: '#405469'
-            }
-        ]
+        return gradient
+    }
+
+    const prepareData = () => {
+        let dataPoints = []
+        let dataLabels = []
+
+        props.data.forEach(element => {
+            dataPoints.push(element.Elo)
+            dataLabels.push(element.time)
+        })
+
+        const data = {
+            labels: dataLabels,
+            datasets: [{ data: dataPoints }]
+        }
+        return data
     }
 
     if (props.data === undefined || props.data.length === 0) {
         return <></>
     } else {
-        return <Line data={data} options={graphOptions}/>
+        return <Line ref={chartRef} data={chartData} options={chartOptions} />
     }
 }
 
