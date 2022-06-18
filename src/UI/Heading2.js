af0abeecd26f368c6df9525dddea3a5bf6718f2c
Johannes Berger <johannes@berger.ac>
2022-06-18 12:46:41 +0200

    Rework heading 2 component

--- src/UI/Heading2.js
+++ src/UI/Heading2.js
@@ -1,6 +1,6 @@
 const Heading2 = (props) => {
     return(
-        <h2 class='text-primary-dark mt-2 mb-4 ml-4 uppercase font-bold text-xl overflow-clip h-8'>{props.children}</h2>
+        <h2 class='text-primary-dark uppercase font-bold text-xl overflow-clip h-8'>{props.children}</h2>
     )
 }
 
