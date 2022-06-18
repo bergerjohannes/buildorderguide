d13e85447d5f9781a29464a8b8ace38a0c942749
Johannes Berger <johannes@berger.ac>
2022-06-18 22:26:14 +0200

    Implement Switch

--- src/Builder/BuilderOverview.js
+++ src/Builder/BuilderOverview.js
@@ -7,6 +7,7 @@ import { Navigate } from "react-router-dom";
 import Heading1 from '../UI/Heading1.js'
 import CenteredButton from '../UI/CenteredButton.js'
 import LoadingIndicator from '../UI/LoadingIndicator.js'
+import Switch from '../UI/Switch.js'
 
 const BuilderOverview = (props) => {
 
@@ -14,6 +15,7 @@ const BuilderOverview = (props) => {
     const [builds, setBuilds] = useState(undefined)
     const [loadallBuilds, setLoadAllBuilds] = useState(false) // ToDO: Implement way for admin to see all builds
     const [redirect, setRedirect] = useState(undefined)
+    const [buildLoadingPreference, setBuildLoadingPreference] = useState(0)
 
     useEffect(() => {
         if (loadallBuilds) {
@@ -44,11 +46,16 @@ const BuilderOverview = (props) => {
 
     if (redirect !== undefined) return <Navigate to={`/builder/build/${redirect}`} />
 
+    const selectBuildLoadingPreferenceOption = (option) => {
+        setBuildLoadingPreference(option)
+    }
+
     return (
         <div>
             <Menu />
             <div class='w-full md:w-1/2 m-auto'>
                 <Heading1>Build Order Builder</Heading1>
+                <div class='flex justify-center w-1/2 mx-auto my-4'><Switch option1={'My builds'} option2={'All builds'} slectedOptionIndex={buildLoadingPreference} selectOption={selectBuildLoadingPreferenceOption} /></div>
                 {builds === undefined && <LoadingIndicator text={'Loading build orders ..'} />}
                 {builds !== undefined && <BuilderOverviewTable builds={builds} />}
                 {builds !== undefined && <CenteredButton onClick={addNewBuild}>New Build</CenteredButton>}
