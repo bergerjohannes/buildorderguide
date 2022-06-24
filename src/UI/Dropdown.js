313b1e3e65a0491d699f7f3d5d45d2a042571db2
Johannes Berger <johannes@berger.ac>
2022-06-25 01:00:00 +0200

    Design improvements

--- src/UI/Dropdown.js
+++ src/UI/Dropdown.js
@@ -28,10 +28,10 @@ const Dropdown = (props) => {
             cursor: 'pointer'
         }),
         singleValue: (provided, state) => {
-            const opacity = state.isDisabled ? 0.5 : 1;
-            const transition = 'opacity 300ms';
-
-            return { ...provided, opacity, transition };
+            const opacity = state.isDisabled ? 0.5 : 1
+            const transition = 'opacity 300ms'
+            const color = '#134e4a'
+            return { ...provided, opacity, transition, color };
         }
     }
 
