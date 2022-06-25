3576fce857d231c1a3817c0fdaff4a7332f629fa
Johannes Berger <johannes@berger.ac>
2022-06-25 21:31:06 +0200

    Show error if build is not able to load

--- src/Builds/BuildDetailView.js
+++ src/Builds/BuildDetailView.js
@@ -3,21 +3,33 @@ import { useParams } from 'react-router-dom'
 import DatabaseService from '../DatabaseService'
 import Menu from '../UI/Menu'
 import BuildView from './BuildView'
+import * as Constants from '../Constants'
+import ErrorView from '../UI/ErrorView'
 
 const BuildDetailView = (props) => {
     let { buildId } = useParams()
     const [build, setBuild] = useState(undefined)
+    const [error, setError] = useState(undefined)
 
     useEffect(() => {
         DatabaseService.loadPublishedBuildWithId(buildId).then(b => {
             setBuild(b)
+        }).catch(e => {
+            setError(Constants.Error.BuildNotExistent)
         })
     }, [])
+
+    if (error === Constants.Error.BuildNotExistent) return (
+        <div class='text-center'>
+            <Menu />
+            <ErrorView title={'Error loading build'} description={'Seems like the build you are looking for doesn\'t exist.'} />
+        </div>
+    )
 
     return (
         <div>
             <Menu />
-            {build && <BuildView build={build}/>}
+            {build && <BuildView build={build} />}
         </div>
     )
 }
