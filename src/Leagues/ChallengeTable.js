66599eab63585d8d9bba391b9e4d80b2a3fadac4
Johannes Berger <johannes@berger.ac>
2022-07-01 01:18:18 +0200

    Mobile optimizations

--- src/Leagues/ChallengeTable.js
+++ src/Leagues/ChallengeTable.js
@@ -2,20 +2,20 @@ const ChallengeTable = (props) => {
     return (
         <div class='text-main-dark'>
             {props.data.players.length > 0 &&
-                <div class='flex w-11/12 md:w-1/2 m-auto'>
-                    <p class='w-2/6'><strong>Player</strong></p>
-                    <p class='w-1/6'><strong>Games</strong></p>
-                    <p class='w-1/6'><strong>Won</strong></p>
-                    <p class='w-1/6'><strong>Lost</strong></p>
-                    <p class='w-1/6'><strong>Points</strong></p>
+                <div class='w-full md:w-1/2 grid grid-cols-9 grid-rows-1 m-auto'>
+                    <p class='col-span-3'><strong>Player</strong></p>
+                    <p class='col-span-2'><strong>Games</strong></p>
+                    <p class='col-span-1'><strong>Won</strong></p>
+                    <p class='col-span-1'><strong>Lost</strong></p>
+                    <p class='col-span-2'><strong>Points</strong></p>
                 </div>}
             {props.data.players.sort((a, b) => b.points - a.points).map((player, index) => (
-                <div class='flex w-11/12 md:w-1/2 m-auto'>
-                    <p class='w-2/6'>{player.name}</p>
-                    <p class='w-1/6'>{player.games} / 37</p>
-                    <p class='w-1/6'>{player.won}</p>
-                    <p class='w-1/6'>{player.lost}</p>
-                    <p class='w-1/6'>{player.won} / 37</p>
+                <div class='w-full md:w-1/2 grid grid-cols-9 grid-rows-1 m-auto'>
+                    <p class='col-span-3'>{player.name}</p>
+                    <p class='col-span-2'>{player.games} / 37</p>
+                    <p class='col-span-1'>{player.won}</p>
+                    <p class='col-span-1'>{player.lost}</p>
+                    <p class='col-span-2'>{player.won} / 37</p>
                 </div>
             ))}
         </div>
