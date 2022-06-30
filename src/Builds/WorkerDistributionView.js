56e14d89bd56cdc59511235dc3dbf1182da067b3
Johannes Berger <johannes@berger.ac>
2022-06-30 23:46:45 +0200

    Improve build view table display on smaller devices

--- src/Builds/WorkerDistributionView.js
+++ src/Builds/WorkerDistributionView.js
@@ -1,12 +1,12 @@
 const WorkerDistributionView = (props) => {
 
     return (
-        <div class='flex space-x-5'>
-            <p class='w-6 h-6 text-center'>{props.wood > 0 && props.wood}</p>
-            <p class='w-6 h-6 text-center'>{props.food > 0 && props.food}</p>
-            {props.showGold && <p class='w-6 h-6 text-center'>{props.gold > 0 && props.gold}</p>}
-            {props.showStone && <p class='w-6 h-6 text-center'>{props.stone > 0 && props.stone}</p>}
-            {props.showBuilder && <p class='w-6 h-6 text-center'>{props.builder > 0 && props.builder}</p>}
+        <div class='flex justify-around w-full'>
+            <p class='w-4 h-4 md:w-6 md:h-6 text-center'>{props.wood > 0 && props.wood}</p>
+            <p class='w-4 h-4 md:w-6 md:h-6 text-center'>{props.food > 0 && props.food}</p>
+            {props.showGold && <p class='w-4 h-4 md:w-6 md:h-6 text-center'>{props.gold > 0 && props.gold}</p>}
+            {props.showStone && <p class='w-4 h-4 md:w-6 md:h-6 text-center'>{props.stone > 0 && props.stone}</p>}
+            {props.showBuilder && <p class='w-4 h-4 md:w-6 md:h-6 text-center'>{props.builder > 0 && props.builder}</p>}
         </div>
     )
 }
