67b685c15a90d8ab97540978cdf3ed00e4e03739
Johannes Berger <johannes@berger.ac>
2022-07-16 00:01:02 +0200

    Improve menu design

--- src/UI/Menu.js
+++ src/UI/Menu.js
@@ -14,8 +14,8 @@ const Menu = (props) => {
     return (
         <div class='sticky top-0 flex justify-between z-30 bg-secondary-light'>
             <Logo/>
-            <div class={isMenuOpen ? 'flex' : 'hidden md:flex'}>
-                <nav class='flex flex-col md:flex-row justify-center md:justify-end md:space-x-4 space-y-6 md:space-y-0 min-h-screen md:min-h-fit fixed z-10 md:relative items-center w-full md:w-auto bg-secondary-light md:bg-transparent left-0 top-0 m-0 md:m-4 p-0'>
+            <div class={isMenuOpen ? 'flex' : 'hidden lg:flex'}>
+                <nav class='flex flex-col lg:flex-row justify-center lg:justify-end lg:space-x-4 space-y-6 lg:space-y-0 min-h-screen lg:min-h-fit fixed z-10 lg:relative items-center w-full lg:w-auto bg-secondary-light lg:bg-transparent left-0 top-0 m-0 lg:m-4 p-0'>
                     <MenuItem linkTo='/'>Builds</MenuItem>
                     <MenuItem linkTo='/uptime'>Uptime</MenuItem>
                     <MenuItem linkTo='/stats'>Stats</MenuItem>
@@ -25,8 +25,8 @@ const Menu = (props) => {
                     {user !== null && <MenuItem linkTo='/profile'>Profile</MenuItem>}
                 </nav>
             </div>
-            <button class={isMenuOpen ? 'hidden' : 'block md:hidden z-40 m-4'} onClick={() => setIsMenuOpen(!isMenuOpen)}><FontAwesomeIcon icon={faBars} /></button>
-            <button class={!isMenuOpen ? 'hidden' : 'block md:hidden fixed z-40 right-0 m-4'} onClick={() => setIsMenuOpen(!isMenuOpen)}><FontAwesomeIcon icon={faXmark} /></button>
+            <button class={isMenuOpen ? 'hidden' : 'block lg:hidden z-40 m-4'} onClick={() => setIsMenuOpen(!isMenuOpen)}><FontAwesomeIcon icon={faBars} /></button>
+            <button class={!isMenuOpen ? 'hidden' : 'block lg:hidden fixed z-40 right-0 m-4'} onClick={() => setIsMenuOpen(!isMenuOpen)}><FontAwesomeIcon icon={faXmark} /></button>
         </div>
     )
 }
