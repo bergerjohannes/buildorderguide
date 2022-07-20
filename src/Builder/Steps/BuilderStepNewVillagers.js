ba166fa5d8020dbb34c5f37b4d09e0000e1bb2b3
Johannes Berger <johannes@berger.ac>
2022-07-20 17:07:10 +0300

    Add option to add buildings to the "New Villagers" step

--- src/Builder/Steps/BuilderStepNewVillagers.js
+++ src/Builder/Steps/BuilderStepNewVillagers.js
@@ -4,6 +4,7 @@ import BuilderStepBuild from './BuilderStepBuild'
 import * as Constants from '../../Constants'
 import BuilderDropdownMedium from '../UI/BuilderDropdownMedium'
 import BuilderDropdownSmall from '../UI/BuilderDropdownSmall'
+import AddOptionButton from '../UI/AddOptionButton'
 
 const BuilderStepNewVillagers = (props) => {
 
@@ -13,6 +14,17 @@ const BuilderStepNewVillagers = (props) => {
 
     const handleClickOnCount = (event) => {
         update(props.step.task, event.value, props.step.buildings)
+    }
+
+    const addBuilding = (event) => {
+        let buildings = props.step.buildings !== undefined ? props.step.buildings : []
+        let type = buildings.length === 0 ? Constants.Building.Blacksmith : Constants.Building.Market
+        let count = 1
+        buildings.push({
+            type: type,
+            count: count
+        })
+        update(props.step.task, props.step.count, buildings)
     }
 
     const update = (task, count, buildings) => {
@@ -26,10 +38,11 @@ const BuilderStepNewVillagers = (props) => {
 
     return (
         <div class='flex space-x-1'>
-            {props.step.buildings !== undefined && <><BuilderStepBuild step={props.step} update={props.update} /><BuilderText>&</BuilderText></>}
+            {(props.step.buildings !== undefined && props.step.buildings.length > 0) && <><BuilderStepBuild step={props.step} update={props.update} /><BuilderText>&</BuilderText></>}
             <BuilderDropdownSmall value={props.step.count} options={BuilderInfoService.getNumericOptions()} onChange={handleClickOnCount} />
             <BuilderText>on</BuilderText>
             <BuilderDropdownMedium value={props.step.task} options={BuilderInfoService.getTaskOptions()} onChange={handleClickOnTask} />
+            {(props.step.buildings === undefined || props.step.buildings.length < 1) && <AddOptionButton onClick={addBuilding} />}
         </div>
     )
 }
