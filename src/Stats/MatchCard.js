68cea5236354e5ec8ab9bf69488d7b6674a9f6fe
Johannes Berger <johannes@berger.ac>
2022-09-11 19:11:53 +0200

    Style match card

--- src/Stats/MatchCard.js
+++ src/Stats/MatchCard.js
@@ -34,18 +34,24 @@ const MatchCardAnalysisView = (props) => {
     const dataForPlayer = props.analysis.players[props.profileId]
     if (dataForPlayer === undefined) return (<></>)
     return (
-        <div class='w-full md:w-4/12 pl-4 flex flex-row'>
-            <h3>Analysis for {dataForPlayer.name}</h3>
-            <p><span>Build: </span><span>{dataForPlayer.build}</span></p>
-            <p><span>Mean geAPM: </span><span>{dataForPlayer.mean_apm}</span></p>
-            {dataForPlayer.age_up_times !== undefined &&
-                <>
-                    {dataForPlayer.age_up_times.feudal !== undefined && <p><span>Feudal: </span><span>{StatsInfoService.toReadableTime(dataForPlayer.age_up_times.feudal)}</span></p>}
-                    {dataForPlayer.age_up_times.castle !== undefined && <p><span>Castle: </span><span>{StatsInfoService.toReadableTime(dataForPlayer.age_up_times.castle)}</span></p>}
-                    {dataForPlayer.age_up_times.imperial !== undefined && <p><span>Imperial: </span><span>{StatsInfoService.toReadableTime(dataForPlayer.age_up_times.imperial)}</span></p>}
-                </>}
+
+        <div class='grid overflow-hidden grid-cols-4 grid-rows-3 gap-4'>
+            <div class='box col-start-1 col-end-5 text-center text-lg'>Analysis for <span class='font-bold'>{dataForPlayer.name}</span></div>
+            <div class='flex justify-around flex-row items-center col-start-1 col-end-5 text-xl'>
+                <div class='flex flex-col justify-center items-center'><span>{dataForPlayer.build}</span><span class='text-xs'>Build</span></div>
+                <div class='flex flex-col justify-center items-center'><span>{dataForPlayer.mean_apm}</span><span class='text-xs'>Mean geAPM</span></div>
+            </div>
+            {dataForPlayer.age_up_times !== undefined && <div class='flex justify-around col-start-1 col-end-5'>
+                {dataForPlayer.age_up_times.feudal !== undefined && <p class='flex justify-center items-center flex-col'><span >{StatsInfoService.toReadableTime(dataForPlayer.age_up_times.feudal)}</span><span class='text-xs'>Feudal</span></p>}
+                {dataForPlayer.age_up_times.castle !== undefined && <p class='flex justify-center items-center flex-col'><span>{StatsInfoService.toReadableTime(dataForPlayer.age_up_times.castle)}</span><span class='text-xs'>Castle</span></p>}
+                {dataForPlayer.age_up_times.imperial !== undefined && <p class='flex justify-center items-center flex-col'><span>{StatsInfoService.toReadableTime(dataForPlayer.age_up_times.imperial)}</span><span class='text-xs'>Imperial</span></p>}
+            </div>}
         </div>
     )
+}
+
+const MatchCardSeparator = (props) => {
+    return <div class='bg-secondary-dark mx-auto my-4 w-11/12 h-0.5 rounded-xl'></div>
 }
 
 const MatchCard = (props) => {
@@ -80,15 +86,15 @@ const MatchCard = (props) => {
     const civPlayer1 = CivInfoService.getCivilizationNameForIndex(match.players[1].civ) !== undefined ? CivInfoService.getCivilizationNameForIndex(match.players[1].civ) : 'Unknown'
 
     return (
-        <div class='flex flex-col md:flex-row justify-start w-11/12 lg:w-1/2 max-w-2xl rounded-2xl mx-auto my-8 bg-secondary-light shadow-sm py-4 text-main-dark'>
+        <div class='flex flex-col justify-start w-11/12 lg:w-1/2 max-w-2xl rounded-2xl mx-auto my-8 bg-secondary-light shadow-sm py-4 text-main-dark'>
             <MatchCardMapView match={match} />
-            <div class='bg-secondary-dark mx-auto my-4 md:my-2 w-11/12 md:w-0.5 h-0.5 md:h-auto rounded-xl'></div>
-            <div class='flex space-x-4 justify-start w-full md:w-8/12 px-10'>
+            <MatchCardSeparator />
+            <div class='flex space-x-4 justify-center w-full px-10'>
                 <MatchCardPlayerResultView matchResult={matchResultPlayer0} profile_id={match.players[0].profile_id} name={match.players[0].name} civ={civPlayer0} rating={match.players[0].rating} />
                 <div class='flex flex-col justify-center'><span>vs.</span></div>
                 <MatchCardPlayerResultView matchResult={matchResultPlayer1} profile_id={match.players[1].profile_id} name={match.players[1].name} civ={civPlayer1} rating={match.players[1].rating} />
             </div>
-            <div class='bg-secondary-dark mx-auto my-4 md:my-2 w-11/12 md:w-0.5 h-0.5 md:h-auto rounded-xl'></div>
+            <MatchCardSeparator />
             {props.analysis !== undefined && <MatchCardAnalysisView profileId={props.profile_id} analysis={props.analysis} />}
             {(props.analysis === undefined && isAnalyzing === false) && <div class='flex justify-center'><Button onClick={() => { setIsAnalzing(true); props.analyzeGame(props.match.match_id) }}>Analyze game</Button></div>}
             {(props.analysis === undefined && isAnalyzing === true) && <div class='flex justify-center'>Analyzing game ...</div>}
