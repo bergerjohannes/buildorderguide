376a3a6b1344cbcb6b00044cc01c74ed9ed7a039
Dennis Keil <dennis.keil10@gmail.com>
2022-10-07 14:12:44 +0200

    implement dark mode

--- src/UI/Heading2.js
+++ src/UI/Heading2.js
@@ -1,6 +1,6 @@
 const Heading2 = (props) => {
     return(
-        <h2 class='text-primary-dark uppercase font-bold text-lg overflow-clip h-8'>{props.children}</h2>
+        <h2 class='text-main-dark uppercase font-bold text-lg overflow-clip h-8'>{props.children}</h2>
     )
 }
 
