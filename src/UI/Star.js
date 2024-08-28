64096731955813efe7e4d43296517fc26d676d03
Zygo <82368713+zygo1@users.noreply.github.com>
2024-08-29 01:59:19 +0300

    small fixes on rating

--- src/UI/Star.js
+++ src/UI/Star.js
@@ -1,8 +1,15 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons'
+import React from "react";
+import PropTypes from "prop-types";
 
-const Star = (props) => {
-    return <div class={props.color === true ? 'text-highlight-light' : 'text-secondary-dark'}><FontAwesomeIcon icon={props.full ? faStar : faStarHalf} /></div>
+const Star = ({color,full}) => {
+    return <div className={color ? 'text-highlight-light' : 'text-secondary-dark'}><FontAwesomeIcon icon={full ? faStar : faStarHalf} /></div>
+}
+
+Star.propTypes = {
+    color: PropTypes.bool.isRequired,
+    full: PropTypes.bool.isRequired,
 }
 
 export default Star
